f78d16693eb5a1db0a9be94ca521dbcc6931288a
Srijan Srivastava <srijan.srivastav2@gmail.com>
2024-01-16 01:48:20 +0800

    Fixing bugs with nvim-tree

--- lua/main/plugins/nvim-tree.lua
+++ lua/main/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 -- import nvim-tree plugin safely
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-  return
+	return
 end
 
 -- recommended settings from nvim-tree documentation
@@ -11,28 +11,29 @@ vim.g.loaded_netrwPlugin = 1
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
 nvimtree.setup({
-    -- change folder arrow icons
-    renderer = {
-        icons = {
-            glyphs = {
-                folder = {
-                    arrow_closed = "", -- arrow when folder is closed
-                    arrow_open = "", -- arrow when folder is open
-                },
-            },
-        },
-    },
-    actions = {
-        open_file = {
-            window_picker = {
-                enable = false,
-            },
-        },
-    },
-    filters = {
-        custom = { ".DS_Store" },
-    },
-    git = {
-        ignore = true,
-    },
+	-- change folder arrow icons
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "", -- arrow when folder is closed
+					arrow_open = "", -- arrow when folder is open
+				},
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	filters = {
+		custom = { ".DS_Store", "^.git$" },
+	},
+	git = {
+		enable = true,
+		ignore = false,
+	},
 })
